b91ee540949510ae38f8b87ead46836c3af5473e
RdubDesignINC <jake.bergren@outlook.com>
2018-09-08 14:51:21 -0400

    Just trying to install a windows service nooo biggy

---
+++ JustAnotherWindowService/MyService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using wcfTest;
+
+namespace JustAnotherWindowService
+{
+    public partial class MyService : ServiceBase
+    {
+        ServiceHost host;
+        public MyService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            host = new ServiceHost(typeof(WCFTestService));
+            host.Open();
+        }
+
+        protected override void OnStop()
+        {
+            host.Close();
+        }
+    }
+}
--- wcfTest/WCFTestService.cs
+++ wcfTest/WCFTestService.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace wcfTest
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-    public class Service1 : IService1
+    public class WCFTestService : IWCFTestService
     {
         public string GetData(int value)
         {
