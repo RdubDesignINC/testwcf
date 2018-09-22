77a98250048c403dee5051bc766bf85102e0f5fb
RdubDesignINC <jake.bergren@outlook.com>
2018-09-22 13:46:14 -0400

    using AppVeyor environment variables

--- consoleTest/deploy.ps1
+++ consoleTest/deploy.ps1
@@ -11,16 +11,17 @@ if($mysched1 -eq $null)
     $headers["Content-type"] = "application/json"
     $build_info = Invoke-RestMethod -Uri 'https://ci.appveyor.com/api/environments/39631/settings' -Headers $headers -Method Get  | Select-Object -ExpandProperty environment
 
+	$user = $build_info[0].settings.environmentVariables | Where-Object name -eq "Admin" | Select-Object -ExpandProperty value | Select-Object value
     $pw = $build_info[0].settings.environmentVariables | Where-Object name -eq "Password" | Select-Object -ExpandProperty value | Select-Object value
 
-    Write-Host $pw.value
+    Write-Host $pw.value " " $user.value
 
-    $A = New-ScheduledTaskAction -Execute "C:\appveyor\projects\testwcf\applications\consoleTestName\bin\Debug\consoleTest.exe"
+    $A = New-ScheduledTaskAction -Execute $env:APPLICATION_PATH + "\bin\Debug\consoleTest.exe"
     $S = New-ScheduledTaskSettingsSet
     $T = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Hours 1)
-    $P = New-ScheduledTaskPrincipal "desktop-dtu1uo9\JacobLenovo"
-    $D = New-ScheduledTask -Description TEST -Action $A -Settings $S -Principal $P -Trigger $T
-    Register-ScheduledTask MySched1 -InputObject $D  #-User JacobLenovo -Password $pw
+    #$P = New-ScheduledTaskPrincipal "desktop-dtu1uo9\JacobLenovo"
+    $D = New-ScheduledTask -Description TEST -Action $A -Settings $S -Trigger $T
+    Register-ScheduledTask MySched1 -InputObject $D  -User $user.value -Password $pw.value
 }
 else
 {
