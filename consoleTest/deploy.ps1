3f5139e32e94449c1c25310969ab3bab1b220c87
RdubDesignINC <jake.bergren@outlook.com>
2018-09-27 19:27:36 -0400

    545

--- consoleTest/deploy.ps1
+++ consoleTest/deploy.ps1
@@ -3,7 +3,7 @@ if($mysched1 -eq $null)
 {
     echo "now you are going to get it my friend"
 
-	$env:myVar | out-file "C:\temp\Output.txt"
+	$env:myVar | out-file "C:\temp\Output2.txt"
 
     $token = 'vhfbd63dvitncm9k7pa0'
     $headers = @{}
